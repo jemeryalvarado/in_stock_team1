68b900d04eedd7db4ccc046ad3b1168c777f035b
Brandon <brandon.redford@outlook.com>
2024-05-23 19:12:03 -0700

    add url for edit warehouse page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import WarehousesPage from "./pages/WarehousesPage/WarehousesPage";
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
 import "./App.scss";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
 
           <Route path="/" element={<WarehousesPage />}/>
           <Route path="/warehouses" element={<WarehousesPage />} />
+          <Route path="warehouses/edit/:warehouseId" element={<EditWarehousePage />} />
           <Route path="/inventories"/>
         </Routes>
       <Footer />
