0c2b410d072a40067c3cc8a7a34d5dd90e3ed3ff
Dina Shouhdy <dshouhdy@gmail.com>
2024-05-25 12:59:26 -0700

    committing inital warehouse changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import WarehousesPage from "./pages/WarehousesPage/WarehousesPage";
+import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage"
 import "./App.scss";
 import EditInventory from "./components/Main/EditInventory";
 
@@ -10,14 +11,10 @@ function App() {
     <Router>
       <Header />
         <Routes>
-          
-         {/* @the person who is in charge of adding the warehouse component>>
-         Please pass the "warehouses" component function as a prop in the "/" route as well.
-         Since "/" is going to be homepage and our homepage is going to be warehouses.
-         */}
-
           <Route path="/" element={<WarehousesPage />}/>
           <Route path="/warehouses" element={<WarehousesPage />} />
+          <Route path="/warehouses/details" element={<Navigate to="/warehouses/details/1"/>} />
+          <Route path="/warehouses/details/:warehouseId" element={< WarehouseDetailsPage />} />
           <Route path="/inventories"/>
           <Route path="/inventories/edit" element={<EditInventory />}/>
         </Routes>
--- src/pages/WarehousesPage/WarehousesPage.js
+++ src/pages/WarehousesPage/WarehousesPage.js
@@ -2,9 +2,21 @@ import "./WarehousesPage.scss";
 import "../../components/Main/EditInventory.scss"
 
 function WarehousesPage(){
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
     return(
         <div className="whitebox">
-         Empty White Box
+            <button onClick={openModal}>Modal</button>
+            <Modal isOpen={modalIsOpen} onClose={closeModal} warehouse_name = {"Washington"} />
         </div>
     )
 }
