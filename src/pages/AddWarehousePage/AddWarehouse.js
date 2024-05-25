c689e1b46fe55df347c4cac7717a1d027c1c8e83
Arian Faridi <arianfaridi@Arians-MBP.home>
2024-05-25 01:25:24 -0700

    form field validation functions and styling added

--- src/pages/AddWarehousePage/AddWarehouse.js
+++ src/pages/AddWarehousePage/AddWarehouse.js
@@ -1,8 +1,24 @@
 import "./AddWarehouse.scss";
 import { useState } from "react";
-import arrow_back from "../../assets/icons/arrow_back-24px.svg"
+import arrow_back from "../../assets/icons/arrow_back-24px.svg";
+import error_icon from "../../assets/icons/error-24px.svg";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+
+//following is a conditional component for rendering the form validation error
+function ShowError({errorMessage, fieldInvalid}){
+  if (fieldInvalid){
+    return (
+      <>
+        <div className="warehouseForm__errorWrapper">
+          <img className="warehouseForm__errorImage" src={error_icon} />
+          <span className="warehouseForm__errorMessage">{errorMessage}</span>
+        </div>
+      </>
+    )
+  };
+};
+
 function AddWarehouse(){
   const baseURL = " http://localhost:8080";
   const [newWarehouse, setWarehouse] = useState({
@@ -16,53 +32,92 @@ function AddWarehouse(){
     contact_email:"",
   });
 
+  const [fieldStatus, setFieldStatus]= useState({});
+
   const navigate = useNavigate();
+
+
+  //following is the function for validating empty fields, as well as email and phone numbers
+  const validate = () =>{
+    const fieldErrors = {};
+    let fieldsValid = true;
+    const phoneValidator = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
+
+    for (const [key,value] of Object.entries(newWarehouse)){
+     if(!value){
+       console.log('empty field for: ', key)
+       fieldsValid = false;
+       fieldErrors[key] = "This field is required";
+     }
+     if(key === 'contact_email' && (!value.includes('@') || !value.includes('.com'))){
+      fieldErrors["email"] = "Email is not valid";
+      fieldsValid =false;
+     };
+     if(key==='contact_phone' && !phoneValidator.test(value)){
+      fieldErrors['phone'] = "Phone number is not valid";
+      fieldsValid =false;
+     };
+    }
+    setFieldStatus(fieldErrors);
+    return fieldsValid;
+   };
+
 
   const submitForm = (event) =>{
     event.preventDefault();
 
-    setWarehouse({
-    warehouse_name:"",
-    address:"",
-    city:"",
-    country:"",
-    contact_name:"",
-    contact_position:"",
-    contact_phone:"",
-    contact_email:"",
-    });
-
-    const postData =() =>{
-      const response = axios
-      .post(`${baseURL}/warehouses`,{
-        warehouse_name: newWarehouse.warehouse_name,
-        address: newWarehouse.address,
-        city: newWarehouse.city,
-        country: newWarehouse.country,
-        contact_name: newWarehouse.contact_name,
-        contact_position: newWarehouse.contact_position,
-        contact_phone: newWarehouse.contact_phone,
-        contact_email: newWarehouse.contact_email,
-      })
-      .then((response)=>{
-        console.log(response.data);
-        return response.data;
-      })
-      .catch((error) =>{
-        console.error(error)
-      });
-      return response;
-    };
-
-    postData();
-    navigate("/")
+    if (validate()){
+      console.log('all fields are good!')
+    } else{
+      console.log('Error in one of fields')
+    }
+ 
+      // setWarehouse({
+      //   warehouse_name:"",
+      //   address:"",
+      //   city:"",
+      //   country:"",
+      //   contact_name:"",
+      //   contact_position:"",
+      //   contact_phone:"",
+      //   contact_email:"",
+      //   });
+    
+        // const postData =() =>{
+        //   const response = axios
+        //   .post(`${baseURL}/warehouses`,{
+        //     warehouse_name: newWarehouse.warehouse_name,
+        //     address: newWarehouse.address,
+        //     city: newWarehouse.city,
+        //     country: newWarehouse.country,
+        //     contact_name: newWarehouse.contact_name,
+        //     contact_position: newWarehouse.contact_position,
+        //     contact_phone: newWarehouse.contact_phone,
+        //     contact_email: newWarehouse.contact_email,
+        //   })
+        //   .then((response)=>{
+        //     console.log(response.data);
+        //     return response.data;
+        //   })
+        //   .catch((error) =>{
+        //     console.error(error)
+        //   });
+        //   return response;
+        // };
+    
+        // postData();
+        // navigate("/")
+    
   };
+
 
   const inputChange = (event) =>{
     event.preventDefault();
     const {name , value} = event.target;
     setWarehouse({...newWarehouse, [name]:value});
   };
+
+  
 
     return (
         <section className="formContainer">
@@ -93,7 +148,8 @@ function AddWarehouse(){
                     className="warehouseForm__fields"
                     value={newWarehouse.warehouse_name}
                     onChange={inputChange}
-                  />
+                    />
+                    <ShowError fieldInvalid={fieldStatus.warehouse_name} errorMessage={fieldStatus.warehouse_name}/>
 
                 <label htmlFor="address" className="warehouseForm__labels">
                     Street Address
@@ -107,6 +163,7 @@ function AddWarehouse(){
                     value={newWarehouse.address}
                     onChange={inputChange}
                   />
+                 <ShowError fieldInvalid={fieldStatus.address} errorMessage={fieldStatus.address}/>
 
                 <label htmlFor="city" className="warehouseForm__labels">
                    City
@@ -120,6 +177,7 @@ function AddWarehouse(){
                     value={newWarehouse.city}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.city} errorMessage={fieldStatus.city}/>
 
                 <label htmlFor="country" className="warehouseForm__labels">
                    Country
@@ -133,6 +191,7 @@ function AddWarehouse(){
                     value={newWarehouse.country}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.country} errorMessage={fieldStatus.country}/>
 
             </div>
             <hr className="divider__middle" />
@@ -151,6 +210,7 @@ function AddWarehouse(){
                     value={newWarehouse.contact_name}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.contact_name} errorMessage={fieldStatus.contact_name}/>
 
                 <label htmlFor="contact_position" className="warehouseForm__labels">
                     Position
@@ -164,6 +224,7 @@ function AddWarehouse(){
                     value={newWarehouse.contact_position}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.contact_position} errorMessage={fieldStatus.contact_position}/>
 
                 <label htmlFor="contact_phone" className="warehouseForm__labels">
                     Phone Number
@@ -177,6 +238,8 @@ function AddWarehouse(){
                     value={newWarehouse.contact_phone}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.phone} errorMessage={fieldStatus.phone}/>
+                  
 
                 <label htmlFor="contact_email" className="warehouseForm__labels">
                     Email
@@ -190,6 +253,7 @@ function AddWarehouse(){
                     value={newWarehouse.contact_email}
                     onChange={inputChange}
                   />
+                  <ShowError fieldInvalid={fieldStatus.email} errorMessage={fieldStatus.email}/>
 
 
             </div>
